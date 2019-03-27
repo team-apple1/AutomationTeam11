c75b02b2059f3bbcd68486af82244dce2ea43550
fazimi1 <fazliazimi@gmail.com>
2019-03-27 19:58:42 -0400

    update

--- BBC/src/test/java/Search/Searchbar.java
+++ BBC/src/test/java/Search/Searchbar.java
@@ -10,14 +10,20 @@ public class Searchbar extends CommonAPI {
     public void searchBar() {
         driver.findElement(By.id("orb-search-q")).sendKeys("climatechange", Keys.ENTER);
     }
+
     @Test
     public void searchButton() {
         driver.findElement(By.id("orb-search-q")).sendKeys("technology");
         driver.findElement(By.id("orb-search-button")).click();
     }
-        @Test
-        public void search() {
-            driver.findElement(By.id("orb-search-q")).sendKeys("weather", Keys.ENTER);
-        }
+
+    @Test
+    public void searchWeather() {
+        driver.findElement(By.id("orb-search-q")).sendKeys("weather", Keys.ENTER);
     }
 
+    @Test
+    public void searchBusiness() {
+        driver.findElement(By.id("orb-search-q")).sendKeys("business", Keys.ENTER);
+    }
+}
